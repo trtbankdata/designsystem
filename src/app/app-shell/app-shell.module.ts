8bd92c254001ff8de1c6802a9373b02ba26b2d3b
Jakob Engelbrecht Olesen <jakob@basher.dk>
2018-10-16 10:27:48 +0200

    Added examples to app-shell

--- src/app/app-shell/app-shell.module.ts
+++ src/app/app-shell/app-shell.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { KirbyModule } from '../../kirby/kirby.module';
 
+import { KirbyModule } from '../../kirby/kirby.module';
+import { ExamplesModule } from '../examples/examples.module';
 import { ROUTES, COMPONENT_DECLARATIONS } from './app-shell.common';
 
 @NgModule({
   imports: [
     CommonModule,
     KirbyModule,
+    ExamplesModule,
     RouterModule.forChild(ROUTES),
   ],
   declarations: [COMPONENT_DECLARATIONS]
