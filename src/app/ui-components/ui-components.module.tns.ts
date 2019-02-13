27206071ff53194737672c98cc03a64e1f5095a7
Jakob Engelbrecht Olesen <jakob@basher.dk>
2019-02-13 14:40:22 +0100

    Add navigation + subpages for tns components

--- src/app/ui-components/ui-components.module.tns.ts
+++ src/app/ui-components/ui-components.module.tns.ts
@@ -1,11 +1,13 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { NativeScriptCommonModule } from 'nativescript-angular/common';
 import { NativeScriptRouterModule } from 'nativescript-angular/router';
+
 import { ROUTES, COMPONENT_DECLARATIONS } from './ui-components.common';
 
 @NgModule({
   imports: [
     NativeScriptCommonModule,
+    NativeScriptRouterModule,
     NativeScriptRouterModule.forChild(ROUTES)
   ],
   declarations: [
