c94e50a9e7a7059070fa1d9c6283703b71ebf256
Alexander Hafstad <alexander@hafstad.dk>
2019-04-01 14:11:05 +0200

    refactor(kirby-list): remove kirby-list-item

--- src/kirby/components/kirby-list/list.common.ts
+++ src/kirby/components/kirby-list/list.common.ts
@@ -1,8 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { ListComponent } from '~/kirby/components/kirby-list/list.component';
-import { KirbyListItemComponent } from '~/kirby/components/kirby-list/item/item.component';
 import { KirbyLabelComponent } from '~/kirby/components/kirby-list/label/label.component';
 
 export const commonImports = [CommonModule];
-export const commentDeclarations = [ListComponent, KirbyListItemComponent, KirbyLabelComponent];
-export const commonExports = [ListComponent, KirbyListItemComponent, KirbyLabelComponent];
+export const commentDeclarations = [ListComponent, KirbyLabelComponent];
+export const commonExports = [ListComponent, KirbyLabelComponent];
