d75d4b3bce701466f9919dcf4c4f83aa510c15a2
Per Høyer Stensnæxs <perhoyer@pers-mbp.jbdeveloper.jyskebank.dk>
2019-04-09 17:33:28 +0200

    first try GroupHeader and ItemGrouping

--- src/kirby/components/kirby-list/list.component.ts
+++ src/kirby/components/kirby-list/list.component.ts
@@ -1,17 +1,32 @@
-import { Component, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
+import { Component, EventEmitter, Input, Output, TemplateRef, OnInit } from '@angular/core';
 
 @Component({
   selector: 'kirby-list-by-ionic',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
 })
-export class ListComponent {
+export class ListComponent implements OnInit{
   @Input() items: any;
+  @Input() propToGroupOn: string;
   @Input() itemTemplate: TemplateRef<any>;
   @Input() headerTemplate: TemplateRef<any>;
   @Input() footerTemplate: TemplateRef<any>;
+  @Input() sectionHeaderTemplate: TemplateRef<any>;
 
   @Output() itemSelect = new EventEmitter<any>();
+
+  private _myGroupingFunc: (item: any) => any;
+  constructor() {
+    this.myGroupingFunc = (item: any) => {
+
+      return item[this.propToGroupOn];
+    };
+  }
+
+  ngOnInit() {
+    // console.log('**********************************************');
+    // console.log('PROP: ', this.propToGroupOn);
+  }
 
   onItemSelect(selectedItem: any) {
     this.itemSelect.emit(selectedItem);
@@ -20,4 +35,14 @@ export class ListComponent {
   trackByFn(index) {
     return index;
   }
+
+  // RadListView - Grouping
+  set myGroupingFunc(value: (item: any) => any) {
+    this._myGroupingFunc = value;
+  }
+
+  get myGroupingFunc(): (item: any) => any {
+      return this._myGroupingFunc;
+  }
+
 }
