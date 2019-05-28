739d8fc6dee52ab8897e980697cbdec55d332c83
tbozhikov <teodor.bozhikov@gmail.com>
2019-05-28 16:46:48 +0300

    reproduce a case where a RadListView itemTap event is swallowed by ModalStack element

--- src/app/examples/modal-example/first-embedded-modal-example/first-embedded-modal-example.component.ts
+++ src/app/examples/modal-example/first-embedded-modal-example/first-embedded-modal-example.component.ts
@@ -4,13 +4,45 @@ import { ModalController } from '@kirbydesign/designsystem/modal';
 import { ModalConfig } from '@kirbydesign/designsystem/modal';
 import { SecondEmbeddedModalExampleComponent } from '../second-embedded-modal-example/second-embedded-modal-example.component';
 import { COMPONENT_PROPS } from '@kirbydesign/designsystem/modal';
+import { ItemEventData } from 'tns-core-modules/ui/list-view/list-view';
 
 @Component({
   templateUrl: './first-embedded-modal-example.component.html',
 })
 export class FirstEmbeddedModalExampleComponent {
   props: { [key: string]: any };
+  countries: { name: string; imageSrc: string }[] = [
+    { name: 'Australia', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/au.png' },
+    { name: 'Belgium', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/be.png' },
+    { name: 'Bulgaria', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/bg.png' },
+    { name: 'Canada', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/ca.png' },
+    { name: 'Switzerland', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/ch.png' },
+    { name: 'China', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/cn.png' },
+    {
+      name: 'Czech Republic',
+      imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/cz.png',
+    },
+    { name: 'Germany', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/de.png' },
+    { name: 'Spain', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/es.png' },
+    { name: 'Ethiopia', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/et.png' },
+    { name: 'Croatia', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/hr.png' },
+    { name: 'Hungary', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/hu.png' },
+    { name: 'Italy', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/it.png' },
+    { name: 'Jamaica', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/jm.png' },
+    { name: 'Romania', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/ro.png' },
+    { name: 'Russia', imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/ru.png' },
+    {
+      name: 'United States',
+      imageSrc: 'https://play.nativescript.org/dist/assets/img/flags/us.png',
+    },
+  ];
+  tappedItemIndex: number;
 
+  onItemTap(args: ItemEventData): void {
+    this.tappedItemIndex = args.index;
+    console.log('Item with index: ' + args.index + ' tapped');
+  }
+  
   constructor(
     @Inject(COMPONENT_PROPS) private componentProps,
     private modalController: ModalController,
