9ed7d0dfd00784086e489617f2edff8349a283c8
Martin Metaksov <mmmtrifork@gmail.com>
2019-06-27 10:38:51 +0200

    initial work on item swipe templates on native

--- src/kirby/components/list/list.component.ts
+++ src/kirby/components/list/list.component.ts
@@ -8,6 +8,8 @@ import {
   Output,
   TemplateRef,
 } from '@angular/core';
+import { ListViewEventData } from 'nativescript-ui-listview';
+import { View } from 'tns-core-modules/ui/page/page';
 
 import {
   ListHeaderDirective,
@@ -131,6 +133,90 @@ export class ListComponent implements OnChanges {
 
   onLoadOnDemand(event?: LoadOnDemandEventData) {
     this.listHelper.onLoadOnDemand(this, event);
+  }
+
+  leftThresholdPassed: boolean = false;
+  rightThresholdPassed: boolean = false;
+
+  public onSwipeActionSelected(args: ListViewEventData) {
+    console.log(`Tapped on a button...`);
+  }
+
+  public onSwipeCellStarted(args: ListViewEventData) {
+    const swipeLimits = args.data.swipeLimits;
+    const swipeView = args['object'];
+    const leftItem = swipeView.getViewById<View>('mark-view');
+    const rightItem = swipeView.getViewById<View>('delete-view');
+    swipeLimits.left = leftItem.getMeasuredWidth();
+    swipeLimits.right = rightItem.getMeasuredWidth();
+    swipeLimits.threshold = leftItem.getMeasuredWidth() / 2;
+  }
+
+  public onSwipeCellFinished(args: ListViewEventData) {
+    const swipeView = args['object'];
+    const leftItem = swipeView.getViewById('mark-view');
+    const rightItem = swipeView.getViewById('delete-view');
+    if (this.leftThresholdPassed) {
+      console.log('Perform left action');
+    } else if (this.rightThresholdPassed) {
+      console.log('Perform right action');
+    }
+    this.leftThresholdPassed = false;
+    this.rightThresholdPassed = false;
+  }
+
+  public onCellSwiping(args: ListViewEventData) {
+    const swipeLimits = args.data.swipeLimits;
+    const swipeView = args['swipeView'];
+    const mainView = args['mainView'];
+    const leftItem = swipeView.getViewById('mark-view');
+    const rightItem = swipeView.getViewById('delete-view');
+
+    if (args.data.x > swipeView.getMeasuredWidth() / 4 && !this.leftThresholdPassed) {
+      console.log('Notify perform left action');
+      const markLabel = leftItem.getViewById('mark-text');
+      this.leftThresholdPassed = true;
+    } else if (args.data.x < -swipeView.getMeasuredWidth() / 4 && !this.rightThresholdPassed) {
+      const deleteLabel = rightItem.getViewById('delete-text');
+      console.log('Notify perform right action');
+      this.rightThresholdPassed = true;
+    }
+    if (args.data.x > 0) {
+      const leftDimensions = View.measureChild(
+        leftItem.parent,
+        leftItem,
+        // layout.makeMeasureSpec(Math.abs(args.data.x), layout.EXACTLY),
+        // layout.makeMeasureSpec(mainView.getMeasuredHeight(), layout.EXACTLY)
+        0,
+        0
+      );
+      View.layoutChild(
+        leftItem.parent,
+        leftItem,
+        0,
+        0,
+        leftDimensions.measuredWidth,
+        leftDimensions.measuredHeight
+      );
+    } else {
+      const rightDimensions = View.measureChild(
+        rightItem.parent,
+        rightItem,
+        // layout.makeMeasureSpec(Math.abs(args.data.x), layout.EXACTLY),
+        // layout.makeMeasureSpec(mainView.getMeasuredHeight(), layout.EXACTLY)
+        0,
+        0
+      );
+
+      View.layoutChild(
+        rightItem.parent,
+        rightItem,
+        mainView.getMeasuredWidth() - rightDimensions.measuredWidth,
+        0,
+        mainView.getMeasuredWidth(),
+        rightDimensions.measuredHeight
+      );
+    }
   }
 
   private createOrderMap(
