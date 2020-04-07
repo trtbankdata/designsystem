45b3417b269620f4daeb2729389c0093b13b150b
Jakob Engelbrecht Olesen <jakob@basher.dk>
2020-04-07 10:38:21 +0200

    :art: Keep format of auto-generated stencil files

--- libs/angular/src/lib/directives/proxies.ts
+++ libs/angular/src/lib/directives/proxies.ts
@@ -1,33 +1,22 @@
 /* tslint:disable */
 /* auto-generated angular directive proxies */
-import {
-  ChangeDetectionStrategy,
-  ChangeDetectorRef,
-  Component,
-  ElementRef,
-  EventEmitter,
-  NgZone,
-} from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from '@angular/core';
 import { fromEvent } from 'rxjs';
 
 export const proxyInputs = (Cmp: any, inputs: string[]) => {
   const Prototype = Cmp.prototype;
-  inputs.forEach((item) => {
+  inputs.forEach(item => {
     Object.defineProperty(Prototype, item, {
-      get() {
-        return this.el[item];
-      },
-      set(val: any) {
-        this.z.runOutsideAngular(() => (this.el[item] = val));
-      },
+      get() { return this.el[item]; },
+      set(val: any) { this.z.runOutsideAngular(() => (this.el[item] = val)); }
     });
   });
 };
 
 export const proxyMethods = (Cmp: any, methods: string[]) => {
   const Prototype = Cmp.prototype;
-  methods.forEach((methodName) => {
-    Prototype[methodName] = function() {
+  methods.forEach(methodName => {
+    Prototype[methodName] = function () {
       const args = arguments;
       return this.z.runOutsideAngular(() => this.el[methodName].apply(this.el, args));
     };
@@ -35,12 +24,12 @@ export const proxyMethods = (Cmp: any, methods: string[]) => {
 };
 
 export const proxyOutputs = (instance: any, el: any, events: string[]) => {
-  events.forEach((eventName) => (instance[eventName] = fromEvent(el, eventName)));
-};
+  events.forEach(eventName => instance[eventName] = fromEvent(el, eventName));
+}
 
 // tslint:disable-next-line: only-arrow-functions
 export function ProxyCmp(opts: { inputs?: any; methods?: any }) {
-  const decorator = function(cls: any) {
+  const decorator =  function(cls: any){
     if (opts.inputs) {
       proxyInputs(cls, opts.inputs);
     }
@@ -52,15 +41,11 @@ export function ProxyCmp(opts: { inputs?: any; methods?: any }) {
   return decorator;
 }
 
-import { Components } from '@kirbydesign/core';
+import { Components } from '@kirbydesign/core'
 
 export declare interface KirbyBadge extends Components.KirbyBadge {}
 
-@Component({
-  selector: 'kirby-badge',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  template: '<ng-content></ng-content>',
-})
+@Component({ selector: 'kirby-badge', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>' })
 export class KirbyBadge {
   protected el: HTMLElement;
   constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
