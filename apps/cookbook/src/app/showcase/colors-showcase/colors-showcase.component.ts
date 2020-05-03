ea27b44c892acab17aec35c8ddf2914c4bc52320
Jakob Engelbrecht <jakob@basher.dk>
2020-05-03 08:58:37 +0200

    📝 Add Dark Mode toggle to color example

--- apps/cookbook/src/app/showcase/colors-showcase/colors-showcase.component.ts
+++ apps/cookbook/src/app/showcase/colors-showcase/colors-showcase.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ChangeDetectorRef } from '@angular/core';
 
 import { ColorHelper, KirbyColor } from '@kirbydesign/designsystem';
 
@@ -7,16 +7,35 @@ import { ColorHelper, KirbyColor } from '@kirbydesign/designsystem';
   templateUrl: './colors-showcase.component.html',
   styleUrls: ['./colors-showcase.component.scss'],
 })
-export class ColorsShowcaseComponent {
+export class ColorsShowcaseComponent implements OnInit {
   selectedColor = 'primary';
   selectedOnColor = 'primary-contrast';
   brandColors = ColorHelper.brandColors;
   notificationColors = ColorHelper.notificationColors;
   systemColors = ColorHelper.systemColors;
   textColors = ColorHelper.textColors;
+  prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
+  darkModeEnabled: boolean;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef, private renderer: Renderer2) {}
 
   onColorClick(color: KirbyColor) {
     this.selectedColor = color.name;
     this.selectedOnColor = color.name + '-contrast';
+  }
+
+  ngOnInit(): void {
+    this.setDarkMode(this.prefersDark.matches);
+    this.prefersDark.addListener((e) => {
+      this.setDarkMode(e.matches);
+      this.changeDetectorRef.detectChanges();
+    });
+  }
+
+  setDarkMode(enabled) {
+    this.darkModeEnabled = enabled;
+    enabled
+      ? this.renderer.addClass(document.body, 'dark')
+      : this.renderer.removeClass(document.body, 'dark');
   }
 }
